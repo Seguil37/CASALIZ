4ffd01823863ee64ca21c1be54234b3528e9b8f7
Jhojan Vidal Seguil Osores <127449470+Seguil37@users.noreply.github.com>
2025-12-11 15:57:54 -0500

    Add public system settings endpoint

--- app/Http/Controllers/Api/SystemSettingsController.php
+++ app/Http/Controllers/Api/SystemSettingsController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Models\SystemSettings;
 use Illuminate\Http\Request;
 
-class SystemSettingsController
+class SystemSettingsController extends Controller
 {
-    //
+    public function public(Request $request)
+    {
+        $query = SystemSettings::query()
+            ->where('is_public', true);
+
+        if ($request->has('group')) {
+            $query->where('group', $request->query('group'));
+        }
+
+        $settings = $query->get()->mapWithKeys(function (SystemSettings $setting) {
+            return [$setting->key => $this->castValue($setting->value, $setting->type)];
+        });
+
+        return response()->json($settings);
+    }
+
+    private function castValue(?string $value, string $type)
+    {
+        return match ($type) {
+            'boolean' => filter_var($value, FILTER_VALIDATE_BOOLEAN),
+            'integer' => (int) $value,
+            'json' => json_decode($value ?? '', true) ?? [],
+            default => $value,
+        };
+    }
 }
--- app/Models/SystemSettings.php
+++ app/Models/SystemSettings.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemSettings extends Model
 {
-    //
+    protected $table = 'system_settings';
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+        'description',
+        'is_public',
+    ];
 }
